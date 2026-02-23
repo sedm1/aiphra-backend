6aa5a802f1520f8b29122841023015f1f046b380
Николай <nikolaj.novikov@topvisor.com>
2026-02-23 18:15:55 +0300

    УДаление тестовых данных

--- public/index.php
+++ public/index.php
@@ -6,11 +6,3 @@ load_env(__DIR__ . '/../.env');
 
 header('Content-Type: application/json; charset=utf-8');
 
-$t = microtime(true);
-$db = dbh();                  // время подключения
-vd(['connect' => microtime(true) - $t]);
-
-$t = microtime(true);
-$db = dbh();                  // время подключения
-vd(['connect' => microtime(true) - $t]);
-
