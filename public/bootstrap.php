be3197400cfa7440198c8e36ca46e589ce49d0ab
Николай <nikolaj.novikov@topvisor.com>
2026-02-26 23:37:48 +0300

    Удално

--- public/bootstrap.php
+++ public/bootstrap.php
@@ -6,16 +6,4 @@ require __DIR__ . '/../src/Globals/Cors.php';
 load_env(__DIR__ . '/../.env');
 apply_cors();
 
-if (session_status() === PHP_SESSION_NONE) {
-    $isSecure = !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off';
-    session_set_cookie_params([
-        'lifetime' => 0,
-        'path' => '/',
-        'secure' => $isSecure,
-        'httponly' => true,
-        'samesite' => 'Lax',
-    ]);
-    session_start();
-}
-
 header('Content-Type: application/json; charset=utf-8');
